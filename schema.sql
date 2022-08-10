dab813a5a95bc6ac81ba4aee084339337e5f9b7b
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-10 22:59:20 +0300

    changes schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,12 @@
-CREATE TABLE animals ( ID int, Name varchar(100), Date_of_birth date, Escape_attempts int, Neutered boolean, Weight_kg decimal );
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals(
+  id            INT GENERATED ALWAYS AS IDENTITY,
+  name    VARCHAR(250), date_of_birth date, 
+  escape_attempts INT, 
+  neutered boolean, 
+  weight_kg decimal,
+  species VARCHAR(250),
+  PRIMARY KEY(id),
+);
 
