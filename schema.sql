e131bded7035c73dfdc87521f1ddd2e12346cfcf
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-10 22:02:09 +0300

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,2 @@
 CREATE TABLE animals ( ID int, Name varchar(100), Date_of_birth date, Escape_attempts int, Neutered boolean, Weight_kg decimal );
 
-ALTER TABLE table_name ADD PRIMARY KEY (column_name);
-
-ALTER TABLE animals DROP COLUMN ID; -----> deletes a specific column(ID)
-
-ALTER TABLE animals ADD ID SERIAL PRIMARY KEY;
-
-ALTER DATABASE vet_clinic DROP TABLE animals; ------> deletes the database's table animals
-
-ALTER TABLE animals 
